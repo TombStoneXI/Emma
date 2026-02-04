edb5fbc5f43b6809a8581ee018675d609a5b459e
TombStoneXI <huang.kevin2@gmail.com>
2026-02-04 17:22:22 +1300

    Update config.js

--- config.js
+++ config.js
@@ -22,10 +22,10 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
+            text: "Is babe a cute bubu?",                                    // First interaction
+            yesBtn: "Small yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "A Massive Yesss! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
